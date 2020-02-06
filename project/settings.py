f4d9f0512677516b4297952dc179ff482b29c8f5
mbsantiago <santiago.mbal@gmail.com>
2020-02-05 20:09:26 -0600

    removed selia forms dependency

--- project/settings.py
+++ project/settings.py
@@ -2,7 +2,6 @@ import os
 
 from irekua_dev_settings.settings import *
 from selia_templates.settings import *
-from selia_forms.settings import *
 from selia_registration.settings import *
 
 
@@ -13,7 +12,6 @@ LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale'), ]
 
 INSTALLED_APPS = (
     SELIA_REGISTRATION_APPS +
-    SELIA_FORMS_APPS +
     SELIA_TEMPLATES_APPS +
     IREKUA_BASE_APPS
 )
